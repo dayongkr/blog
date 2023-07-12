c49fc40f4b76cd7265b6046a7403159ffa4389e1
dayongkr <dayongkr@gmail.com>
2023-07-12 14:58:58 +0900

    feat(fe): sitemap.xml

---
+++ app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+import { getAllPosts } from "@/lib/api";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const posts = getAllPosts(['slug', 'date']).map((post) => {
+    // TODO: 시간도 고려하기
+    const date = post.date.replaceAll('.', '').split(' ').map((d) => parseInt(d))
+    return {
+      url: `https://dayong.dev/posts/${post.slug}`,
+      lastModified: new Date(date[0], date[1] - 1, date[2])
+    }
+  })
+  return [{ url: 'https://dayong.dev', lastModified: new Date() }, ...posts]
+}
